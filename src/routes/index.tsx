84ac32e5c6418f7b0431a0f3f73969b05f367251
orderlyapps <167072703+orderlyapps@users.noreply.github.com>
2025-05-26 08:57:16 +1000

    Initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import { createFileRoute, Link } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+  component: App,
+});
+
+function App() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        padding: "2rem",
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+      }}
+    >
+      <code style={{ fontSize: "2rem" }}>Playground</code>
+      <Link to="/mantine/buttons">Mantine</Link>
+    </div>
+  );
+}
